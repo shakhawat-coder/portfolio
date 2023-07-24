f5bfa92ddc7475f6511e42b36a69b309566135a5
shakhawat <shakhawathossen188@gmail.com>
2023-07-24 11:03:54 +0600

    Home section design

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function(){
+    var typed = new Typed(".typed", {
+        strings: ["Shakhawat"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 3000,
+        showCursor: false,
+        loop:true
+      });
+
+      var typed = new Typed(".typed2", {
+        strings: ["Frontend Developer"],
+        typeSpeed: 100,
+        // backSpeed: 100,
+        startDelay: 2000,
+        showCursor: false,
+        fadeOut: true,
+        fadeOutClass: 'typed-fade-out',
+        fadeOutDelay: 2000,
+        loop:true
+      });
+});
